a6480a140481b0b867779639bd721c444309f97e
CuberQAQ <a224503353@163.com>
2024-11-27 00:19:49 +0800

    update

--- jsx-runtime.d.ts
+++ jsx-runtime.d.ts
@@ -1,4 +1,8 @@
-import { InsideIntrinsicElements } from "./src/index.js";
+import { LayoutManagerFactory } from "./types"
+import { NativeWidgetAttributesTypeMap } from "./types/platform/zeppos"
+
+
+export declare type InsideIntrinsicElements = LayoutManagerFactory.AttributesMap & NativeWidgetAttributesTypeMap
 
 export namespace JSX {
     export type IntrinsicElements = InsideIntrinsicElements
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,9 +2,7 @@ import { LayoutManagerFactory } from './layout/index.js'
 
 export * from './layout/index.js'
 
-export declare type InsideIntrinsicElements = LayoutManagerFactory.AttributesMap & {
-            
-}
+
 // declare global {
 //     export namespace JSX {
 //         export type IntrinsicElements = InsideIntrinsicElements
