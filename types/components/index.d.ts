128d272641b2484e435bfe78cc47351e7aa08c7d
CuberQAQ <1851445251@qq.com>
2025-01-01 23:54:52 +0800

    update

--- types/components/index.d.ts
+++ types/components/index.d.ts
@@ -1,4 +1,2 @@
-import { LayoutManagerFactory } from './layout/index.js';
 export * from './layout/index.js';
-export declare type InsideIntrinsicElements = LayoutManagerFactory.AttributesMap & {};
 //# sourceMappingURL=index.d.ts.map
--- types/decorator/debug.d.ts
+++ types/decorator/debug.d.ts
@@ -4,7 +4,10 @@
  * @param context
  * @returns
  */
-export declare function log(printArgs?: boolean, logger?: (...data: any[]) => void): (originalMethod: any, context: ClassMethodDecoratorContext) => (this: any, ...args: any[]) => any;
+export declare function log(printArgs?: boolean, logger?: {
+    (...data: any[]): void;
+    (message?: any, ...optionalParams: any[]): void;
+}): (originalMethod: any, context: ClassMethodDecoratorContext) => (this: any, ...args: any[]) => any;
 /**
  * **（类装饰器）显示字符串标签 在toString时显示[object 类名]而非[object Object]**
  * @param value 操作的类
